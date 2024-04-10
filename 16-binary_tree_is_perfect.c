1b2fb08cb18a902ec1789369efe12ca6904495fb
NickelannG <8836@holbertonstudents.com>
2024-04-10 05:32:00 +0000

    Task 16 fix: consdiered if node was root then its a perfect tree

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -47,7 +47,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (1);
 	if (ldepth == rdepth && binary_tree_is_perfect(tree->left) &&
 		binary_tree_is_perfect(tree->right))
-		return (1);
+		return (ldepth + 1);
 
 	return (0);
 }
