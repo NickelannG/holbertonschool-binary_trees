73f8ac8983f927223956ec4908657ca38d6d4ec4
NickelannG <8836@holbertonstudents.com>
2024-04-10 07:42:35 +0000

    Task 16: included variables

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -36,14 +36,18 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	int hleft, hright;
+
 	if (tree == NULL)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
 		return (1);
 
-	if (binary_tree_height(tree->left) !=
-	(binary_tree_height(tree->right)))
+	hleft = binary_tree_height(tree->left);
+	hright = binary_tree_height(tree->right);
+
+	if (hleft != hright)
 	{
 		return (0);
 	}
