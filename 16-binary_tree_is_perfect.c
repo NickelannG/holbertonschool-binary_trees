1780ade8fa9b2027eae7662a63703599965e2fdf
NickelannG <8836@holbertonstudents.com>
2024-04-10 06:23:22 +0000

    Task 16: so far

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,6 +1,32 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_is_full - checks if a binary tree is full
+ *
+ * @tree: a pointer ot the root node
+ *
+ * Return: 1 if its full, or 0 if its not or if tree is NULL
+ */
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	/* Leaf */
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left != NULL && tree->right != NULL)
+	{
+		/* Check the subtrees */
+		return (binary_tree_is_full(tree->left) &&
+				binary_tree_is_full(tree->right));
+	}
+	else
+		return (0);
+}
+
+/**
  * binary_tree_depth - Measures the deapth of a node in a binary tree
  *
  * @tree: a pointer to the node
@@ -35,19 +61,20 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	/* Check if tree only has one node */
+	/* Means tree only has one node*/
 	if (tree->left == NULL && tree->right == NULL)
-		return (1); /* one node means its balance */
+		return (1); /* one node means its perfect */
 
 	ldepth = binary_tree_depth(tree->left);
 	rdepth = binary_tree_depth(tree->right);
 
-	if (ldepth == rdepth && binary_tree_is_perfect(tree->left) &&
-		binary_tree_is_perfect(tree->right) && tree->parent == NULL)
-		return (1);
-	if (ldepth == rdepth && binary_tree_is_perfect(tree->left) &&
-		binary_tree_is_perfect(tree->right))
-		return (ldepth + 1);
-
+	if (binary_tree_is_full(tree))
+	{
+		if (ldepth == rdepth)
+		{
+			if (tree->left == NULL && tree->right == NULL)
+				return (1);
+		}
+	}
 	return (0);
 }
