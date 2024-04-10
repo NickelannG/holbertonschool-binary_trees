31b493fa1b891e72e2a76846c9c8eb87acd46a38
NickelannG <8836@holbertonstudents.com>
2024-04-10 05:26:36 +0000

    Task 16 fix: return value for failing case

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -43,7 +43,8 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	rdepth = binary_tree_depth(tree->right);
 
 	if (ldepth == rdepth && binary_tree_is_perfect(tree->left) &&
-		binary_tree_is_perfect(tree->right))
+		binary_tree_is_perfect(tree->right) && tree->parent == NULL)
 		return (1);
+
 	return (0);
 }
